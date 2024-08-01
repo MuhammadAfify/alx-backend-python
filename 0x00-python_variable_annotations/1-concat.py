d7a10ea55f4022d11e8815dd2700dc3095879ed2
Muhammad Afify <muhammadafefy@gmail.com>
2024-08-01 23:38:21 +0000

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes a string"""
+
+def concat(str1: str, str2: str) -> str:
+    """takes two strings and return concatenated"""
+    return str1 + str2
