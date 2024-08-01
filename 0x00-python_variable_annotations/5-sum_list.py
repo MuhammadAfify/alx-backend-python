b8da25eec636f0751e3939104418b6669c85fc97
Muhammad Afify <muhammadafefy@gmail.com>
2024-08-01 23:55:48 +0000

    Python - Variable Annotations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """type-annotated function sum_list which takes a list input_list of floats"""
 
-from type import list
-def sum_list(input_list: list[float]) -> float:
+from typing import list
+
+def sum_list(input_list: List[float]) -> float:
     """returns their sum as a float"""
     a: float = 0.0
     for idx in input_list:
         a += idx
     return a
-
